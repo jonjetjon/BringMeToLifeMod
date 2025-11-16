b3bff4f22d5293a36e64e6a582d8201bf47ff32b
Kobe Thuy <thuynguyentrungdang@gmail.com>
2025-11-16 17:43:46 +0700

    Fix another Fika remnants in Core and added Patch

---
+++ RevivalMod-Fika/Patches/ConsumeDefibItemPatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using EFT;
+using EFT.InventoryLogic;
+using Fika.Core.Main.Players;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using RevivalMod.Features;
+
+namespace RevivalMod.Patches;
+
+[HarmonyPatch(typeof(RevivalFeatures))]
+[HarmonyPatch(nameof(RevivalFeatures.ConsumeDefibItem))]
+public class ConsumeDefibItemPatch
+{
+    [PatchPrefix]
+    private static void PatchPrefix(Player ___player, Item ___defibItem)
+    {
+        try
+        {
+            if (___player is not FikaPlayer)
+                return;
+
+            FikaPlayer fikaPlayer = (FikaPlayer)___player;
+            
+            InventoryController inventoryController = fikaPlayer.InventoryController;
+            GStruct153 discardResult = InteractionsHandlerClass.Discard(___defibItem, inventoryController, true);
+
+            if (discardResult.Failed)
+            {
+                Plugin.LogSource.LogError($"Couldn't remove item: {discardResult.Error}");
+                return;
+            }
+                
+            inventoryController.TryRunNetworkTransaction(discardResult, result =>
+            {
+                if (result.Failed)
+                    Plugin.LogSource.LogError($"inventoryController.TryRunNetworkTransaction failed: {result.Error}");
+            });
+        }
+        catch (Exception ex)
+        {
+            Plugin.LogSource.LogError($"Error consuming defib item: {ex.Message}");
+        }
+    }
+}
